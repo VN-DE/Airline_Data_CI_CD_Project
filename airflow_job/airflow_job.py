7f2439efb490a578da66b155833d623cf3c8dba8
vidyanand <vndataeng@gmail.com>
2025-03-15 23:47:35 +0530

    cluster name simplified

--- airflow_job/airflow_job.py
+++ airflow_job/airflow_job.py
@@ -14,7 +14,7 @@ default_args = {
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2024, 12, 14),
+    'start_date': datetime(2025, 3, 14),
 }
 
 # Define the DAG
